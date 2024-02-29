a7af9f80fde546a9a4a867295b1609d1c46a5e95
Mira Jill Yumul <86516574+MiraJill@users.noreply.github.com>
2024-02-29 16:04:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function concatenateValues(a, b) //declare a function
+{
+    return a.concat(b); //use ‘concat’ method to join 2 array lists
+}
+console.log(concatenateValues(['a','b','c'], [1,2,3])); //show output in console
+
+const sayAge = (age) => console.log(`My age is ${age}`);
+
+sayAge(22);
